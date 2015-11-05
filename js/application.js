81062f439d8d422efc9ea86c5b67326c4800211e
Scott Miller <scott.miller.utah@gmail.com>
2015-11-04 21:37:35 -0700

    Added demo-link event
    Signed-off-by: Scott Miller <scott.miller.utah@gmail.com>

--- js/application.js
+++ js/application.js
@@ -8,4 +8,9 @@ $(document).ready(function(){
           scrollTop: $(scrollTarget).offset().top-40},'slow');
         });
   });
+
+  $(".demo-link").click(function(){
+    ga('send', 'event', 'Click', 'Demo')    
+  });
+
 });
